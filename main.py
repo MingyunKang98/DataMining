243433ee5397cb9ce54ff61484aa9990dd3c3d5f
MingyunKangSeoultech <kmg54312@gmail.com>
2023-10-11 11:51:53 +0900

    231011

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
+import matplotlib.pyplot as plt
+# 누적막대그래프
+x = [1,2,3,4,5]
+y1 = [6,7,8,2,4]
+y2 = [7,8,2,4,2]
+plt.bar(x,y1, label='bar1')
+plt.bar(x,y2, label='bar2', bottom=y1)
+plt.legend()
+plt.show()
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import pandas as pd
+# 박스플랏
+data = [[2, 4, 6, 8, 10],[6,7, 8, 2, 4],[1, 3, 5, 7, 9],[7, 8, 2, 4, 2]]
+x = pd.DataFrame(data)
+plt.boxplot(x)
+plt.show()
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# 영역그래프
+days = [1, 2, 3, 4, 5]
+sleeping = [7, 8, 6, 11, 7]
+eating = [2, 3, 4, 3, 2]
+working = [7, 8, 7, 2, 2]
+playing = [8, 5, 7, 8, 13]
+plt.stackplot(days, sleeping, eating, working, playing)
+plt.legend(labels=['sleeping', 'eating', 'working', 'playing'])
+plt.show()
